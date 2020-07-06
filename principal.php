4e8e94d8d0baa2c38da6ee46416d9ffb3515c7bf
alejandroclerc <62072849+alejandroclerc@users.noreply.github.com>
2020-07-06 15:26:23 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -1,4 +1,9 @@
 <?php
+	/*-------------------------
+	Autor: Alejandro Clerc
+	Web: www
+	Mail: alejandroclerc@gmail.com
+	---------------------------*/
 	session_start();
 	if (!isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] != 1) {
         header("location: login.php");
@@ -9,7 +14,7 @@
 	require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
 	require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
 	
-	$active_productos="active";
+	$active_pasilloss="active";
 	$active_carpas="active";
 	
 	$title="C.B. | BALNEARIO PRINCESA";
@@ -29,44 +34,87 @@
 		<div class="panel-heading">
 		    <div class="btn-group pull-right">
 				<button type='button' class="btn btn-success" data-toggle="modal" data-target="#nuevaCarpa"><span class="glyphicon glyphicon-plus" ></span> Agregar Carpa</button>
+			
 			</div>
-			<h4><i class='glyphicon glyphicon-search'></i> CARPAS</h4>
+			<h4><i class='glyphicon glyphicon-home'></i> CARPAS</h4>
+			
+			
 		</div>
 		<div class="panel-body">
 		
 			
 			
 			<?php
-			include("modal/registro_carpas.php");
+			include("modal/registro_carpa.php");
 			include("modal/editar_carpas.php");
 			?>
 			<form class="form-horizontal" role="form" id="datos">
 				
 						
 				<div class="row">
-					<div class='col-md-4'>
-						<label>Filtrar por Nro. de Carpa</label>
+					<div class='col-md-2'>
+						<label>Numero de Carpa</label>
+						
 						<input type="text" class="form-control" id="q" placeholder="Ingrese Numero" onkeyup='load(1);'>
 					</div>
 					
-					<div class='col-md-4'>
-						<label>Filtrar por Pasillo</label>
-						<select class='form-control' name='id_pasillo' id='id_pasillo' onchange="load(1);">
-							<option value="">Seleccione un pasillo</option>
+					<div class='col-md-2'>
+						<label>Filtrar x Carpa</label>
+						<select class='form-control' name='id_carpa' id='carpa_id' onchange="load(1);">
+							<option value="">Todas</option>
+							
 							<?php 
-							$query_pasillo=mysqli_query($conn,"select * from pasillos order by nombre_pasillo");
-							while($rw=mysqli_fetch_array($query_pasillo))	{
+							$query_cliente=mysqli_query($conn,"select * from carpas order by id_carpa");
+							while($rw=mysqli_fetch_array($query_cliente))	{
 								?>
-							<option value="<?php echo $rw['id_pasillo'];?>"><?php echo $rw['nombre_pasillo'];?></option>			
+							<option value="<?php echo $rw['id_carpa'];?>"><?php echo $rw['id_carpa'];?></option>
 								<?php
 							}
 							?>
 						</select>
+					</div>
+					<div class='col-md-2'>
+						<label>Numero de Pasillo</label>
+						
+						<input type="text" class="form-control" id="q" placeholder="Ingrese Numero" onkeyup='load(2);'>
+					</div>
+					<div class='col-md-2'>
+						<label>Filtrar x Pasillo</label>
+				
+						<select class='form-control' name='id_pasillo' id='pasillo_id' onchange="load(2);">
+							<option value="">Todos</option>
+							
+							<?php 
+							$query_cliente=mysqli_query($conn,"select * from pasillos order by id_pasillo");
+							while($rw=mysqli_fetch_array($query_cliente))	{
+								?>
+							<option value="<?php echo $rw['id_pasillo'];?>"><?php echo $rw['nombre_pasillo'];?></option>
+								<?php
+							}
+							?>
+						</select>	
+						
 						
 					</div>
-					<div class='col-md-4'>
-						<label>TOTAL DE OCUPANTES EN EL BALNEARIO</label>
+					<div class='col-md-2'>
+						<label>TOTAL DE CARPAS</label>
+						<?php
+							$query_carpa1=mysqli_query($conn,"select * from carpas");
+						?>
+						<br><button type='button' class="btn btn-success" <label><?php echo $query_carpa1->num_rows;?></label> </button>
 						
+						
+					</div>
+					<div class='col-md-2'>
+						<label>CANT. PERSONAS</label>
+						<?php
+							$query_carpa1=mysqli_query($conn,"select * from carpas");
+						?>
+						<br><button type='button' class="btn btn-success" <label><?php echo $query_carpa1->num_rows;?></label> </button>
+						
+						
+					</div>
+					
 						
 					</div>
 					<div class='col-md-12 text-center'>
@@ -100,26 +148,11 @@
 	include("footer.php");
 	?>
 	<script type="text/javascript" src="js/carpas.js"></script>
+	
   </body>
 </html>
+
 <script>
-function eliminar (id){
-		var q= $("#q").val();
-		var id_pasillo= $("#id_pasillo").val();
-		$.ajax({
-			type: "GET",
-			url: "./ajax/buscar_carpas.php",
-			data: "id="+id,"q":q+"id_pasillo="+id_pasillo,
-			 beforeSend: function(objeto){
-				$("#resultados").html("Mensaje: Cargando...");
-			  },
-			success: function(datos){
-			$("#resultados").html(datos);
-			load(1);
-			}
-		});
-	}
-		
 	$(document).ready(function(){
 			
 		<?php 
@@ -131,25 +164,5 @@ function eliminar (id){
 		
 		?>	
 	});
-		
-$( "#guardar_carpa" ).submit(function( event ) {
-  $('#guardar_datos').attr("disabled", true);
-  
- var parametros = $(this).serialize();
-	 $.ajax({
-			type: "POST",
-			url: "ajax/nueva_carpa.php",
-			data: parametros,
-			 beforeSend: function(objeto){
-				$("#resultados_ajax_carpas").html("Mensaje: Cargando...");
-			  },
-			success: function(datos){
-			$("#resultados_ajax_carpas").html(datos);
-			$('#guardar_datos').attr("disabled", false);
-			load(1);
-		  }
-	});
-  event.preventDefault();
-})
-
 </script>
+
