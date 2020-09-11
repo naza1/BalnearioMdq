319a32263fc3c91b2ca27f392f7b8a9034063ea5
alejandroclerc <62072849+alejandroclerc@users.noreply.github.com>
2020-09-10 21:36:30 -0300

    Update setearOcupantes.php
    Cambios 10/09

--- ajax/setearOcupantes.php
+++ ajax/setearOcupantes.php
@@ -7,10 +7,18 @@
   $pass1 = $_POST['user_password'];
   $sqlCarpa = "SELECT user_password_hash FROM Users WHERE user_name = 'admin'";
   $pass = R::getRow($sqlCarpa);
+<<<<<<< HEAD
   
   if (password_verify($pass1, $pass['user_password_hash']))
   {
     echo "Las contraseñas coinciden.";
+=======
+
+  if (password_verify($pass1, $pass['user_password_hash']))
+  {
+    echo "Las contraseñas coinciden.";
+    R::exec("UPDATE Carpas SET ocupacion_actual=0;");
+>>>>>>> df2343bfb207afc6dc788c066efcf7a6c9f18892
   }
   else
   {
